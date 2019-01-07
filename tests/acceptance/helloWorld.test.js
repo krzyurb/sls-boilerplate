f4bf9988542ade452b159bc0694c68312166ef6e
krzyurb <11517908+krzyurb@users.noreply.github.com>
2019-01-07 15:08:19 +0100

    Extract base acceptance test url

--- tests/acceptance/helloWorld.test.js
+++ tests/acceptance/helloWorld.test.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+import baseEndpoint from './baseEndpoint';
+
 describe('Hello world handler', () => {
   test('responds with hello message', async () => {
-    const response = await axios.get(`${process.env.TEST_URL}/hello-world`);
+    const response = await axios.get(`${baseEndpoint}/hello-world`);
 
     expect(response.status).toEqual(200);
     expect(response.data).toEqual({ message: 'Hello world!' });
